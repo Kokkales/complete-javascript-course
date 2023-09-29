1d06193f97d5876c815255af02b3d8572cdc96e6
Konstantinos Kokkalis <kokkaleskonstantinos@gmail.com>
2023-09-29 15:31:19 +0300

    08,09

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,130 @@
-'use strict';
+// 'use strict';
+
+// // function calcAge(birthYear) {
+// //   const age = 2037 - birthYear;
+
+// //   function printAge() {
+// //     let output = `${firstName} You are ${age}, born in ${birthYear}`;
+// //     console.log(output);
+
+// //     if (birthYear >= 981 && birthYear <= 1996) {
+// //       var millenial = true;
+// //       const firstName = 'Steven';
+// //       const str = `Oh, you are a milenial, ${firstName}`;
+// //       console.log(str);
+
+// //       function add(a, b) {
+// //         return a + b;
+// //       }
+// //       output = 'NEW OUTPUT';
+// //     }
+
+// //     // console.log(add(2, 3));
+
+// //     // console.log(str);
+// //     console.log(millenial);
+// //     console.log(output);
+// //   }
+// //   printAge();
+// //   return age;
+// // }
+
+// // const firstName = 'Jonas';
+// // calcAge(1991);
+
+// // console.log(me);
+// // // console.log(job);
+// // console.log(year);
+
+// // var me = 'Jonas'; //hoisted nut with value of undefined
+// // let job = 'teacher'; //not hoisted
+// // const year = 1991; //not hoisted
+
+// //Function
+// // console.log(addDecl(2, 3));
+// // // console.log(addExpr(2, 3));
+// // console.log(addArrow(2, 3));
+
+// // function addDecl(a, b) {
+// //   //hoisted
+// //   return a + b;
+// // }
+
+// // var addExpr = function (a, b) {
+// //   return a + b;
+// // };
+
+// // var addArrow = (a, b) => {
+// //   a + b;
+// // };
+
+// // console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge; //borrow the fuction
+// console.log(matilda);
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// console.log(f);
+// f();
+
+// var firstName = 'Matilda';
+
+// const jonas = {
+//         firstName: 'Jonas',
+//         year: 1991,
+//         calcAge: function() {
+//             console.log(this);
+//             console.log(2037 - this.year);
+
+//             const isMilenial = function() {
+//                 console.log(this.year >= 1981 && this.year <= 1996)
+//                 isMilenial();
+//             };
+//         }
+//     },
+//     greet: () => console.log(`Hey ${this.firstName}`),
+//     // greet: function () {
+//     //   console.log(`Hey ${this.firstName}`);
+//     // },
+// };
+
+// jonas.greet();
+// jonas.calcAge();
+// ?How to copy obkect
+
+const jesica = {
+  firstName: 'Jesica',
+  lastName: 'Wiliwams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const JesicaCopy = Object.assign({}, jesica); //shallow copy
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,24 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 1,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieced ${this.starterMenu[starterIndex]} ${this.mainMenu[mainIndex]} will be delivered at ${time} to    ${address} `
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious Pasta with ${ing1},${ing2},${ing3}`);
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +44,180 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPizza: function (mainIngredient, ...otheringredients) {
+    console.log(`Main: ${mainIngredient}`);
+    console.log(`Main: ${otheringredients}`);
+  },
 };
+
+for (const day of Object.keys(openingHours)) {
+  console.log(day);
+}
+
+// console.log({ restaurant.openingHours.mon ? .open });
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) {
+//   console.log(item);
+// }
+
+// for (const item of menu.entries()) {
+//   console.log(item);
+// }
+
+// const rest1 = {
+//     name: 'Capri',
+//     numGuests: 0,
+// };
+
+// const rest2 = {
+//     name: 'La Pizza',
+//     owner: 'Giovani Rossi',
+// };
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+// rest1.numGuests || = 10;
+// rest2.numGuests || = 10;
+// console.log(rest1);
+// console.log(rest2);
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// const guestsCorrect = restaurant.numGuests ? ? 10;
+// console.log(guestsCorrect);
+
+// console.log(3 || 'jonas');
+// console.log('' || 'jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('musrooms');
+// const arr = [1, 2, ...[3, 4]];
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(others);
+
+// const [pizza, , risoto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risoto, otherFood);
+
+// const { sat, ...weeekdays } = restaurant.openingHours;
+// console.log(sat, weeekdays);
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1);
+
+// const x = [23, 5, 7];
+// add(...x);
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole,21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantname,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantname, hours, tags);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// //Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, main] = restaurant.order(2, 0);
+// console.log(starter, main);
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArray = [1, 2, ...arr];
+// console.log(newArray);
+
+// console.log(...newArray);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(menu);
+
+// const str = 'Jonas';
+// const letters = [...str, ' ', 'S.'];
+// console.log(letters);
+// console.log(...str);
+// console.log(`${...str} Schemtmann`);
+
+// const ingredients = [
+//   prompt(`Let's make pasta! Ingredient1?`),
+//   prompt(`Let's make pasta! Ingredient2?`),
+//   prompt(`Let's make pasta! Ingredient3?`),
+// ];
+
+// restaurant.orderPasta(...ingredients);
+
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+
+// console.log(newRestaurant);
